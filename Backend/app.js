85cdc913895a89a38e84c609fc263ab40eb81d14
VijaiMegala <115264037+VijaiMegala@users.noreply.github.com>
2024-11-14 20:44:15 +0530

    Update app.js
    changed the port assignment for deployment purpose.

--- Backend/app.js
+++ Backend/app.js
@@ -59,5 +59,5 @@ app.delete("/books/:id", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`$Server running on port ${PORT}`));
